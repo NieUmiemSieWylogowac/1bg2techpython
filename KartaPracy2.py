f3fc1382edba0ed984bd0971c18f997b1b249159
Tordpizza02 <114918421+Tordpizza02@users.noreply.github.com>
2022-10-10 11:19:01 +0000

    Dokończono kartę pracy 2

---
+++ KartaPracy2.py
@@ -0,0 +1,73 @@
+#zad 1
+#a = int(input("Podaj liczbe"))
+#if a % 3 == 0:
+   #print("TAK!")
+#else:
+  #print("NIE! >:|")
+
+#zad 2
+#a = int(input("Zapodaj liczbę: "))
+#if a>=100 and a<1000 and a%17==0:
+ #print("TAK")
+#else:
+ #print("NIE")
+
+#zad 3
+#wiek = int(input("Ile masz lat?\n"))
+#if wiek >= 18:
+ #print("TAK")
+#else
+ #print("NIE")
+
+#zad 4
+#LIMIT = 20
+#waga = int(input("Ile waży ten gruzowóz?"))
+#if waga<=LIMIT:
+#  print("A jedz")
+#else:
+ # print("Bezapelacyjny STOP")
+
+#ankieta
+#a = int(input("Wprowadź pierwszą liczbę"))
+#b = int(input("Wprowadź drugą liczbę"))
+#if a<=b
+  #print("Tak")
+#else:
+#if b<=a
+  #print("Nie")
+#else:
+#if a=b
+  #print("Równe")
+
+#zad 5
+#a = int(input())
+#b = int(input())
+#c = int(input())
+#if(a < c and b > c) or (a > c and b < c):
+  #print ("TAK")
+#else:
+  #print ("NIE")
+
+#zad 6
+
+#a = int(input())
+#p = int(input())
+#ip (a**p - a) % p == 0:
+#print("TAK, spełnia MTF")
+#else:
+#print("NIE, nie spełnia MTF")
+
+#zad 7
+#p = int(input())
+#k = int(input())
+#s = int(input())
+
+#if p + 3*s >= k:
+ # print("Tak")
+  #print(s(s*3+p)-k)
+#else:
+  
+
+
+
+  
