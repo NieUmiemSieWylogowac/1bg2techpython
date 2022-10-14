d2f818692ec1df6f1bdb9e2f13f3873e2d7aef68
Tordpizza02 <114918421+Tordpizza02@users.noreply.github.com>
2022-10-14 17:15:50 +0000

    zrobiono 5 zadan

---
+++ KartaPracy 2a.py
@@ -0,0 +1,37 @@
+#zad 1
+# a,b = int(input()),int(input())
+# if (a + b) % 2 == 0:
+#   print("TAK")
+# else:
+#   print("NIE")
+
+#zad 2
+# a=int(input())
+# g=int(input())
+# if (a+g%2)==(a*g):
+#   print("TAK")
+# else:
+#   print("NIE")
+
+#zad 3
+# k=int(input())
+# l=int(input())
+# m=int(input());
+# if(k == l and k < m and l < m) or (l == m and l < k and m < k ) or (k == m and k < l and m < l):
+#   print("TAK")
+# else:
+#   print("NIE")
+
+#zad 4
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# print(min(a,b,c,d))
+
+#zad 5
+a=int(input())
+b=int(input())
+c=int(input())
+if()
+
