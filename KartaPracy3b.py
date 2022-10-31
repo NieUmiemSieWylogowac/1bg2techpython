d4f8b1b355d5b3f98b8c019ca3637d1b818a0414
Tordpizza02 <114918421+Tordpizza02@users.noreply.github.com>
2022-10-31 20:41:50 +0000

    zrobiono zadania 1-7

--- KartaPracy3b.py
+++ KartaPracy3b.py
@@ -11,5 +11,32 @@
 #   print (i-379, end=" ")
 
 #zad 4
-for i in range()
+# for i in range(110, 999, 11):
+#   print (i-11, end=" ")
+
+#zad 5
+# n = int(input("Wpisz ilość liczb"))
+# suma=0       
+# for  i in range(n):
+#  suma = suma + n
+# print(suma)
+
+#zad 6
+# k = int(input())
+# a = 0
+# for i in range(0,2*k,2):
+#   a = a + i
+# print(a)
+
+#zad 7
+m = int(input())
+a = 0
+for i in range(10,1*m,1):
+  a = a + i
+print(a)
+
+
+
+  
+
 
