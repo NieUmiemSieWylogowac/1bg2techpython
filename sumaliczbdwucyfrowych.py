c0e903ac13d71b0bad8d1eb93da477d86712d8a7
Tordpizza02 <114918421+Tordpizza02@users.noreply.github.com>
2023-01-18 12:19:53 +0000

    Listy1

---
+++ sumaliczbdwucyfrowych.py
@@ -0,0 +1,13 @@
+suma=0
+for i in range(10,100):
+ suma=suma + i
+print(suma)
+
+# Napisz program, który zsumuje k liczb wpisanych przez usera.
+
+# k = int(input())
+# suma = 0
+# for i in range(k):
+#   x = int(input())
+#   suma = suma + x
+# print(suma)
