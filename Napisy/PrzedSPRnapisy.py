28e267a3a4d7b8a0e5a3770792226dd4669dc36f
Tordpizza02 <114918421+Tordpizza02@users.noreply.github.com>
2023-03-27 11:15:09 +0000

    PrzedSprnapisy

---
+++ Napisy/PrzedSPRnapisy.py
@@ -0,0 +1,29 @@
+#1. Wczytaj dowolny napis i wypisz z niego pierwszą i ostatnią literkę
+# napis = int(input())
+# print(a[0], a[-1])
+
+#2. Wczytaj dowolny napis i wypisz z niego literki bez pierwszej i ostatniej
+
+# b = input()
+# for i in range(1, len(b)-1):
+#   print(b[i], end="")
+# print()
+
+# print(b[1:len(b)-1])
+
+# L = list(b)
+# l.pop(0)
+# l.pop(-1)
+# b ="".join(L)
+# print(b)
+
+
+# ZAGADNIENIA NA SPRAWDZIAN NAPISY
+# - len for foreach ord chr
+# - funkcje - sort-
+# - indeksy, zakresy
+# - konwersje list <-> napis
+# - list - sort reverse
+# - split, join
+# - Algorytmy - anagram, palindrom, Boyer-Moore
+
